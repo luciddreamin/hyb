2aeeaf096e079390bdc4fc371eae5e4c476bc6d5
luciddreamin <33933120+luciddreamin@users.noreply.github.com>
2020-12-18 18:40:24 +0530

    Initial upload of the spring boot application to connect to Connect api

---
+++ investalgo/src/test/java/com/rodu/algo/trading/investalgo/InvestalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodu.algo.trading.investalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestalgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
